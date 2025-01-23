9ff3a34826d30f22233561fbe8f888572848dabd
Dagim <dagimawiengidawork371@gmail.com>
2025-01-23 15:08:25 +0300

    code clean up

--- src/components/PrivateNavBar.jsx
+++ src/components/PrivateNavBar.jsx
@@ -16,13 +16,17 @@ const PrivateNavBar = () => {
   };
   return (
     <nav className="primary-link">
+      <div className="logo-container">
+        <h1 className="logo-text">Blogs</h1>
+      </div>
       <NavLink to="/">Home</NavLink>
-     {(auth.role ===1 ||auth.role===2) && <NavLink to="/category">Categories</NavLink>}
+      {(auth.role === 1 || auth.role === 2) && (
+        <NavLink to="/category">Categories</NavLink>
+      )}
       <NavLink to="/posts">Posts</NavLink>
       <NavLink to="/profile">Profile</NavLink>
       <NavLink to="/setting">Setting</NavLink>
       <NavLink to="/login" onClick={handleLogout}>
-        
         Logout
       </NavLink>
     </nav>
